5fb6e041389a6646cdeb8cedce7ae76b8f370dc1
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2015-06-05 10:05:04 -0500

    update

--- register_docker_image_for_service_all.sh
+++ register_docker_image_for_service_all.sh
@@ -5,10 +5,10 @@ set -e
 
 
 # mg-rast-nginx (note that nginx and confd use the same image)
-curl -L http://127.0.0.1:4001/v2/keys/service_images/mg-rast-nginx/shock -XPUT -d value="shock.metagenomics.anl.gov/node/16bd3a55-abd7-4483-a6aa-73bf76d818d4"
+curl -L http://127.0.0.1:4001/v2/keys/service_images/mg-rast-nginx/shock -XPUT -d value="shock.metagenomics.anl.gov/node/a34d112c-5f29-479d-af72-783b3410f264"
 
 # mg-rast-confd (note that nginx and confd use the same image)
-curl -L http://127.0.0.1:4001/v2/keys/service_images/mg-rast-confd/shock -XPUT -d value="shock.metagenomics.anl.gov/node/16bd3a55-abd7-4483-a6aa-73bf76d818d4"
+curl -L http://127.0.0.1:4001/v2/keys/service_images/mg-rast-confd/shock -XPUT -d value="shock.metagenomics.anl.gov/node/a34d112c-5f29-479d-af72-783b3410f264"
 
 
 # solr-m5nr
