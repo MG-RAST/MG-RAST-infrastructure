33654e132c7c2ca6093d7af5150e2aa9177a0b67
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2015-06-08 10:54:24 -0500

    services to start production environment

---
+++ services_start.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -x
+set -e
+
+cd fleet-units
+
+# nginx
+fleetctl start mg-rast-nginx@1.service
+fleetctl start mg-rast-confd@1.service
+
+
+#production web
+#fleetctl start mg-rast-v4-web{,-discovery}@{1..3}.v4.service
+
+
+
+fleetctl start solr-m5nr@1.service
+
+fleetctl start solr-metagenome@1.service
