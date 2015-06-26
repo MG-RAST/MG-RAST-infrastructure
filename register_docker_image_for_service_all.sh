8cf87eef4924bdc9c8eb7006c43eab7c2b3425ba
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2015-06-26 14:44:02 -0500

    add comments

--- register_docker_image_for_service_all.sh
+++ register_docker_image_for_service_all.sh
@@ -55,6 +55,8 @@ curl -L http://127.0.0.1:4001/v2/keys/service_images/awe-server/shock -XPUT -d v
 
 # develop services:
 # fleetctl start mg-rast-v4-web{,-discovery}@{1..2}.v4-web.service
+# fleetctl start mg-rast-v4-web{,-discovery}@{1..2}.v4-web-dev.service
 # fleetctl start mg-rast-v3-web{,-discovery}@{1..2}.v3-web.service
+# fleetctl start mg-rast-v3-web{,-discovery}@{1..2}.v3-web-dev.service
 # fleetctl start cadvisor.service
 
