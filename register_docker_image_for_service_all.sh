4ef5462d04593c1d6c9f1cf1b4ddec6e70153807
Travis Harrison <guumby@gmail.com>
2015-09-14 17:06:26 -0500

    update image

--- register_docker_image_for_service_all.sh
+++ register_docker_image_for_service_all.sh
@@ -54,7 +54,7 @@ curl -L http://127.0.0.1:4001/v2/keys/service_images/awe-server/shock -XPUT -d v
 curl -L http://127.0.0.1:4001/v2/keys/service_images/memcached/shock -XPUT -d value="shock.metagenomics.anl.gov/node/cc8df996-0682-461f-b1f3-616c23cb433e"
 
 #cassandra node
-curl -L http://127.0.0.1:4001/v2/keys/service_images/cassandra/shock -XPUT -d value="shock.metagenomics.anl.gov/node/1e1bfc72-91d4-4d36-bb99-4c819cd989da"
+curl -L http://127.0.0.1:4001/v2/keys/service_images/cassandra/shock -XPUT -d value="shock.metagenomics.anl.gov/node/f5dfa9a3-d1f8-4aff-b123-40e763f2723a"
 
 #opscenter server
 curl -L http://127.0.0.1:4001/v2/keys/service_images/opscenter/shock -XPUT -d value="shock.metagenomics.anl.gov/node/a030ea5b-3971-43c6-8574-6f82c510fca2"
