0cd7507d3fceb276466dd08675bde379c9372c89
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2015-09-22 14:09:29 -0500

    fix name

--- scripts/install-dockbuild.sh
+++ scripts/install-dockbuild.sh
@@ -4,6 +4,6 @@ set -x
 rm -f /home/core/dockbuild
 docker pull mgrast/dockbuild
 docker rm -f dockbuild
-docker create --name dockbuild dockbuild
+docker create --name dockbuild mgrast/dockbuild
 docker cp dockbuild:/app/dockbuild /home/core/
 docker rm -f dockbuild
