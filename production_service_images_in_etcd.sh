7e11675bf60d3c0d38e903e8f0552adc744ca437
Travis Harrison <guumby@gmail.com>
2015-10-01 11:17:31 -0500

    new images

--- production_service_images_in_etcd.sh
+++ production_service_images_in_etcd.sh
@@ -37,9 +37,9 @@ curl -L http://127.0.0.1:4001/v2/keys/service_images/mg-rast-v4-web-v4-web-dev/s
 
 # mg-rast-v3-web-[channel]
 #channel: v3-web
-curl -L http://127.0.0.1:4001/v2/keys/service_images/mg-rast-v3-web-v3-web/shock -XPUT -d value="shock.metagenomics.anl.gov/node/364a381c-a926-45a5-b58a-b5895410af30"
+curl -L http://127.0.0.1:4001/v2/keys/service_images/mg-rast-v3-web-v3-web/shock -XPUT -d value="shock.metagenomics.anl.gov/node/69239822-61e1-4da6-a21d-6c8e198250cc"
 #channel: v3-web-dev
-curl -L http://127.0.0.1:4001/v2/keys/service_images/mg-rast-v3-web-v3-web-dev/shock -XPUT -d value="shock.metagenomics.anl.gov/node/364a381c-a926-45a5-b58a-b5895410af30"
+curl -L http://127.0.0.1:4001/v2/keys/service_images/mg-rast-v3-web-v3-web-dev/shock -XPUT -d value="shock.metagenomics.anl.gov/node/69239822-61e1-4da6-a21d-6c8e198250cc"
 
 #api-server-[channel]
 #channel: api
