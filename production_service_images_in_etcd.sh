ef3829347c5d5477b17e1e1f7fc33cefe83660bb
Travis Harrison <guumby@gmail.com>
2016-01-12 10:12:34 -0600

    fix image

--- production_service_images_in_etcd.sh
+++ production_service_images_in_etcd.sh
@@ -51,7 +51,7 @@ curl -L http://127.0.0.1:4001/v2/keys/service_images/api-server-api-dev/shock -X
 #awe server
 curl -L http://127.0.0.1:4001/v2/keys/service_images/awe-server/shock -XPUT -d value="shock.metagenomics.anl.gov/node/915c04ff-3d4b-4273-b1bf-c006b3483c68"
 #awe client
-curl -L http://127.0.0.1:4001/v2/keys/service_images/awe-server/shock -XPUT -d value="shock.metagenomics.anl.gov/node/915c04ff-3d4b-4273-b1bf-c006b3483c68"
+curl -L http://127.0.0.1:4001/v2/keys/service_images/awe-client/shock -XPUT -d value="shock.metagenomics.anl.gov/node/915c04ff-3d4b-4273-b1bf-c006b3483c68"
 
 #memcached server
 curl -L http://127.0.0.1:4001/v2/keys/service_images/memcached/shock -XPUT -d value="shock.metagenomics.anl.gov/node/cc8df996-0682-461f-b1f3-616c23cb433e"
