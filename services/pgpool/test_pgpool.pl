7ac9eeab293a5a03637c59961192626a459c94c4
Travis Harrison <guumby@gmail.com>
2016-03-23 11:13:02 -0500

    more pgpool stuff

---
+++ services/pgpool/test_pgpool.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use DBI;
+use Getopt::Long;
+
+my $cdir = "";
+my $host = "";
+my $user = "";
+my $pswd = "";
+
+GetOptions (
+    'cdir=s' => \$cdir,
+    'host=s' => \$host,
+    'user=s' => \$user,
+    'pswd=s' => \$pswd 
+);
+
+my $certs = "sslcert=$cdir/postgresql.crt;sslkey=$cdir/postgresql.key";
+my $dbname = "mgrast_analysis";
+
+my $dbh = DBI->connect("DBI:Pg:dbname=$dbname;host=$host", $user, $pswd, { RaiseError => 1, AutoCommit => 0, PrintError => 0 }) || die $DBI::errstr;
+
+my @names = $dbh->tables;
+sort @names
+foreach (@names) { print $_."\n"; }
+
+$dbh->disconnect;
