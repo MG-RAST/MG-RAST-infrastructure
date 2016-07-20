2eaa1436477aa9dc2f84aff1e342436136f1d1f6
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2016-07-20 10:30:18 -0500

    adjust verbosity

--- scripts/deploy_static_docker.sh
+++ scripts/deploy_static_docker.sh
@@ -5,17 +5,19 @@
 # The docker binary that comes with CoreOS is currently not statically compiled.
 
 set -e
-set -x
+
 
 TARGET_DIR="/media/ephemeral"
 
 DOCKER_VERSION=$(/usr/bin/docker --version | grep -o '[0-9]*\.[0-9]*\.[0-9]')
 
 if [ ! -e ${TARGET_DIR}/docker-${DOCKER_VERSION} ] ; then
+    set -x
     rm -f ${TARGET_DIR}/docker-${DOCKER_VERSION}_part
     curl -o ${TARGET_DIR}/docker-${DOCKER_VERSION}_part --retry 10 https://get.docker.com/builds/Linux/x86_64/docker-${DOCKER_VERSION}
     chmod +x ${TARGET_DIR}/docker-${DOCKER_VERSION}_part
     mv ${TARGET_DIR}/docker-${DOCKER_VERSION}_part ${TARGET_DIR}/docker-${DOCKER_VERSION}
+    set +x
     echo "Downloaded: ${TARGET_DIR}/docker-${DOCKER_VERSION}"
 else
     echo "found ${TARGET_DIR}/docker-${DOCKER_VERSION}"
