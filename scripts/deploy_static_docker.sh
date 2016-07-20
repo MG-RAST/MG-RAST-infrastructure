46b187e73d5a7930107168d9f9802d353ce3aa1a
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2016-07-20 10:24:47 -0500

    script to download docker binary

---
+++ scripts/deploy_static_docker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+# This script will download a statically compiled docker binary, which can be easilt mounted into containers. 
+# The docker binary that comes with CoreOS is currently not statically compiled.
+
+set -e
+set -x
+
+TARGET_DIR="/media/ephemeral"
+
+DOCKER_VERSION=$(/usr/bin/docker --version | grep -o '[0-9]*\.[0-9]*\.[0-9]')
+
+if [ ! -e ${TARGET_DIR}/docker-${DOCKER_VERSION} ] ; then
+    rm -f ${TARGET_DIR}/docker-${DOCKER_VERSION}_part
+    curl -o ${TARGET_DIR}/docker-${DOCKER_VERSION}_part --retry 10 https://get.docker.com/builds/Linux/x86_64/docker-${DOCKER_VERSION}
+    chmod +x ${TARGET_DIR}/docker-${DOCKER_VERSION}_part
+    mv ${TARGET_DIR}/docker-${DOCKER_VERSION}_part ${TARGET_DIR}/docker-${DOCKER_VERSION}
+    echo "Downloaded: ${TARGET_DIR}/docker-${DOCKER_VERSION}"
+fi
