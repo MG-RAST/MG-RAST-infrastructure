dbadf10ee52eb5e72fda176c07652c74fcbd273d
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2016-10-28 15:31:45 -0500

    new api image

--- production_service_images_in_etcd.sh
+++ production_service_images_in_etcd.sh
@@ -37,7 +37,7 @@ curl -L http://127.0.0.1:2379/v2/keys/service_images/mg-rast-v3-web-v3-web-dev/s
 #channel: api
 curl -L http://127.0.0.1:2379/v2/keys/service_images/api-server-api/shock -XPUT -d value="shock.metagenomics.anl.gov/node/ca5c00b4-fec5-4943-a0d4-d5f50d9356f2"
 #channel: api-dev
-curl -L http://127.0.0.1:2379/v2/keys/service_images/api-server-api-dev/shock -XPUT -d value="shock.metagenomics.anl.gov/node/d2d74e90-5a0f-4dde-8cab-591d93407d41"
+curl -L http://127.0.0.1:2379/v2/keys/service_images/api-server-api-dev/shock -XPUT -d value="shock.metagenomics.anl.gov/node/45c943fd-7201-452f-831c-1ebf960c927a"
 
 #awe server
 #curl -L http://127.0.0.1:2379/v2/keys/service_images/awe-server/shock -XPUT -d value="shock.metagenomics.anl.gov/node/d1058716-af9b-4aba-9821-b631988e575c"
