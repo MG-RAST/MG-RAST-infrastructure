29750206d0e8aa2cc2bf572c55bbfe547993117f
Tobias Paczian <paczian@mcs.anl.gov>
2017-01-27 05:17:48 -0600

    updated v4

--- production_service_images_in_etcd.sh
+++ production_service_images_in_etcd.sh
@@ -21,8 +21,8 @@ curl -L http://127.0.0.1:2379/v2/keys/service_images/solr-metagenome/shock -XPUT
 
 # mg-rast-v4-web
 #v4
-curl -L http://127.0.0.1:2379/v2/keys/service_images/mg-rast-v4-web-v4-web/shock -XPUT -d value="shock.metagenomics.anl.gov/node/621df8f8-c4bd-4594-825b-3c0d62da32d5"
-#curl -L http://127.0.0.1:2379/v2/keys/service_images/mg-rast-v4-web-v4-web/shock -XPUT -d value="shock.metagenomics.anl.gov/node/13b2b78e-b957-4d7c-920a-caf229ff07c4"
+#curl -L http://127.0.0.1:2379/v2/keys/service_images/mg-rast-v4-web-v4-web/shock -XPUT -d value="shock.metagenomics.anl.gov/node/621df8f8-c4bd-4594-825b-3c0d62da32d5"
+curl -L http://127.0.0.1:2379/v2/keys/service_images/mg-rast-v4-web-v4-web/shock -XPUT -d value="shock.metagenomics.anl.gov/node/42e169b2-ee71-4515-a8d4-e839919b7a46"
 
 #v4-dev
 curl -L http://127.0.0.1:2379/v2/keys/service_images/mg-rast-v4-web-v4-web-dev/shock -XPUT -d value="shock.metagenomics.anl.gov/node/dc5e8c04-be69-46df-a958-f5c9d4b22d68"
