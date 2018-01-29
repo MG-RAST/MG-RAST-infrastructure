d9f9c63e1bd5ffd4a122d765f11704d6498c2da6
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2018-01-29 11:10:54 -0600

    better error handling

--- services/mgrast-message-system/mms-email/mms-email.py
+++ services/mgrast-message-system/mms-email/mms-email.py
@@ -11,9 +11,12 @@ import time
 
 
 
+debug = 1
 
+connection = None
+credentials = None
 # get config
-stream = open('/config/config.yml', 'r') 
+stream = open('/config.yml', 'r') 
 conf = yaml.load(stream)
  
 if not "email" in conf:
@@ -47,12 +50,27 @@ def send_email(subject, msg_text):
     s.quit()
     
 
-
+def makeConnection():
+    global connection
+    global credentials
+    success = 0
+    while success == 0:
+        try:
+            connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq', credentials=credentials))
+        except Exception as e:
+            print("error in building connection: {}".format(str(e)))
+            time.sleep(5)
+            continue
+        success = 1
+        
+        
+    return
 # ------------------------------------------
 
 
 if not "rabbitmq" in conf:
-     sys.exit(1)
+    print("not rabbitmq in conf")
+    sys.exit(1)
 
 rabbitmq = conf["rabbitmq"]
 
@@ -62,59 +80,77 @@ rabbitmq_password = rabbitmq['password']
 
 # setup RabbitMQ connection
 
-credentials = pika.PlainCredentials(rabbitmq_user, rabbitmq_password)
- 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq', credentials=credentials))
+credentials=pika.PlainCredentials(rabbitmq_user, rabbitmq_password)
+
+connection=makeConnection(credentials)
+
  
 channel = connection.channel()
 queue_name ='email_outgoing'
 channel.queue_declare(queue=queue_name)
-
+channel.queue_purge(queue=queue_name) # get rid of old email requests
  
  
 while True:
+    method_frame = None
     
-    method_frame, header_frame, body = channel.basic_get(queue = queue_name, no_ack=False)
-    if method_frame:
+    try:
+        method_frame, header_frame, body = channel.basic_get(queue = queue_name, no_ack=False)
+    except Exception as e:
+        print("error in basic_get: {}".format(str(e)))
+        makeConnection()
+        time.sleep(5)
+        continue
+        
+    
+    if not method_frame:
+        time.sleep(1)
+        continue    
+    
+        
+    if debug:
         print(method_frame, header_frame, body)
         print(body)
-        try:
-            channel.basic_ack(method_frame.delivery_tag)
-        except exception as e:
-            print("error: %s" % str(e), file=sys.stderr)
+        
+    try:
+        channel.basic_ack(method_frame.delivery_tag)
+    except exception as e:
+        print("error: %s" % str(e), file=sys.stderr)
+        time.Sleep(1)
+        continue
+        
+    event = json.loads(body)
+
+    if 'event_type' in event:
+        event_type = event['event_type']
+        if event_type != "email":
+            print("error: event_type unknown, email expexted, got %s" % event_type, file=sys.stderr)
             time.Sleep(1)
             continue
-            
-        event = json.loads(body)
+    else:
+        print("error: event_type unknown, service_test expexted, got nothing")
+        time.Sleep(1)
+        continue
     
-        if 'event_type' in event:
-            event_type = event['event_type']
-            if event_type != "email":
-                print("error: event_type unknown, email expexted, got %s" % event_type, file=sys.stderr)
-                time.Sleep(1)
-                continue
-        else:
-            print("error: event_type unknown, service_test expexted, got nothing")
-            time.Sleep(1)
-            continue
+    if not "subject" in event:
+        print("error: email subject missing", file=sys.stderr)
+        time.Sleep(1)
+        continue
+    
+    if not "message" in event:
+        print("error: email message missing", file=sys.stderr)
+        time.Sleep(1)
+        continue
         
-        if not "subject" in event:
-            print("error: email subject missing", file=sys.stderr)
-            time.Sleep(1)
-            continue
-        
-        if not "message" in event:
-            print("error: email message missing", file=sys.stderr)
-            time.Sleep(1)
-            continue
-            
-        subject = event["subject"]
-        message = event["message"]
-        
-        try:
-            send_email(subject, message)
-        except Exception as e:
-            print("error: send_email: %s" % (str(e)), file=sys.stderr)
+    subject = event["subject"]
+    message = event["message"]
+    
+    try:
+        print("would send email now: %s" % (subject))
+        print(message)
+        #send_email(subject, message)
+    except Exception as e:
+        print("error: send_email: %s" % (str(e)), file=sys.stderr)
 
 
 
