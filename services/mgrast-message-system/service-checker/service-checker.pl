edec81336e2a67b8200fc0cb044df6fda004e9f4
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2018-02-14 10:43:13 -0600

    up timeout for etcd

--- services/mgrast-message-system/service-checker/service-checker.pl
+++ services/mgrast-message-system/service-checker/service-checker.pl
@@ -230,7 +230,7 @@ sub check_etcdcluster {
     
     my $resource = "etcdcluster";
     my $ua = LWP::UserAgent->new;
-    $ua->timeout(60); # etcd has timeout issues...
+    $ua->timeout(60*5); # etcd has timeout issues...
     
     # http://metagenomics.anl.gov:2379/health
     
