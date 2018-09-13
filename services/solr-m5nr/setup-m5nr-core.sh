7625550e55ce78dec26810d0109df0ab8320dd24
Travis Harrison <guumby@gmail.com>
2018-09-13 11:07:59 -0500

    update setup

--- services/solr-m5nr/setup-m5nr-core.sh
+++ services/solr-m5nr/setup-m5nr-core.sh
@@ -2,7 +2,6 @@
 
 # set default value
 M5NR_VERSIONS="1 10"
-ADD_NAME=1
 
 # binary location from http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
 BIN=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
@@ -20,7 +19,6 @@ set -x
 
 if [ "$1" != "" ]; then
     M5NR_VERSIONS=$1
-    ADD_NAME=0
 fi
 
 for i in $M5NR_VERSIONS
@@ -31,11 +29,7 @@ do
     echo ""
 
     cp -av /opt/solr/server/solr/configsets/sample_techproducts_configs /opt/solr/server/solr/m5nr_${M5NR_VERSION}
-    
-    if [ "${ADD_NAME}" -eq 1 ]; then
-        echo "name=m5nr_${M5NR_VERSION}" > /opt/solr/server/solr/m5nr_${M5NR_VERSION}/core.properties
-    fi
-    
+    echo "name=m5nr_${M5NR_VERSION}" > /opt/solr/server/solr/m5nr_${M5NR_VERSION}/core.properties
     cp schema.xml /opt/solr/server/solr/m5nr_${M5NR_VERSION}/conf/schema.xml
     cp solr.in.sh /opt/solr/bin
     mkdir -p /mnt/m5nr_${M5NR_VERSION}/data
